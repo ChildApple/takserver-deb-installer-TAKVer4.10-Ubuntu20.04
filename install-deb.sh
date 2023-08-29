e4fccf02dfb37ea5ed10f351848af068cb0465d0
Pablo Perez Alvarez <PabloAPAlvarez@gmail.com>
2023-08-29 02:23:35 -0500

    Update install-deb.sh

--- install-deb.sh
+++ install-deb.sh
@@ -495,7 +495,7 @@ read HOSTNAME
   echo "What is your email? - Needed for Letsencrypt Alerts"
   read EMAIL
 
-  if certbot certonly --standalone -d $DOMAIN -m $EMAIL --agree-tos --non-interactive; then
+  if certbot certonly --standalone -d $DOMAIN -m $EMAIL --agree-tos --preferred-challenges dns; then
     echo "Certificate obtained successfully!"
     CERT_NAME=$(sudo certbot certificates | grep -oP "(?<=Certificate Name: ).*")
   else
